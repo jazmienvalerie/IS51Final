f8f9b9fa6fbc46ef7f90b6f24b603986c17ac8ba
Jazmien <jazmienb247@gmail.com>
2022-05-19 16:07:01 -0700

    finished structured-english

---
+++ is51_final.py
@@ -0,0 +1,15 @@
+
+
+""" STRUCTURED ENGLISH
+This program is trying to show the number of grades, the average grade, 
+and the percentage of grades that are above the average grade of 83.25. 
+
+The main function will be used to start the application and we will use the 
+calculate_percent_above_average function to find the percentage of the grades that 
+are higher than the average grade. 
+
+The number of grades will be shown as the length of a list of numbers
+The average grade will be shown as sum of all the grades, divided by the number of all grades.
+The percentage of grades above the average grade will be shown as the grades > average grade.
+
+"""
