52f3f804685abe086182437b5da28c6fc52f1ebd
Jazmien <jazmienb247@gmail.com>
2022-05-19 17:09:55 -0700

    finished exam-end and update psuedo-code

--- is51_final.py
+++ is51_final.py
@@ -18,15 +18,29 @@ The percentage of grades above the average grade will be shown as the grades > a
 Create list
 len(list)
 
-scores 
-iterator, accumulator = 0
-loop through scores 
-    accumulator = accumulator + scores 
-    iterator = iterator + 1 
-average = accumulator / total score 
-print average
+calculate average 
+sum of list / len(list)
 
 percentage of grades higher than average scores 
  sum of highscores / 24 * 100
  print percentage 
 """
+#num_of_grades= len(78,67,56,99,80,83,82,91,94,95,77,88,85,92,91,79,88,82,81,86,94,93,92,45)
+
+grades = [78,67,56,99,80,83,82,91,94,95,77,88,85,92,91,79,88,82,81,86,94,93,92,45]
+num_of_grades = len(grades)
+print("The number of grades is: ", len(grades))
+
+def cal_average(grades):
+    sum_grades = 0
+    for t in grades:
+        sum_grades = sum_grades + t
+    avg = sum_grades / len(grades)
+    return avg 
+print ("The average is ", cal_average ([78,67,56,99,80,83,82,91,94,95,77,88,85,92,91,79,88,82,81,86,94,93,92,45]))
+
+sum_of_highergrades = [99,91,94,95,88,85,92,91,88,86,94,94,92]
+num_of_highergrades = len(99,91,94,95,88,85,92,91,88,86,94,94,92)
+percentage_of_highergrades = ((sum_of_highergrades/ 1998) * 100)
+
+print(" The percentage of grades that are above of the average grade is" (percentage_of_highergrades))
